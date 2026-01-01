c2e252eacf287242c71f7b62d6ce10858a3e6e38
Rohullah <rohullahqasemi549@gmail.com>
2026-01-01 22:18:06 +0330

    Day 3: Connected Routes and Server entry point

---
+++ server/index.js
@@ -0,0 +1,19 @@
+    const express = require('express');
+    const cors = require('cors');
+    const dotenv = require('dotenv');
+    const connectDB = require('./config/db');
+
+    dotenv.config();
+    connectDB();
+
+    const app = express();
+    const PORT = process.env.PORT || 5000;
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.use('/api/auth', require('./routes/authRoutes'));
+
+    app.get('/', (req, res) => res.send('✅ DarsYar API is Running!'));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
